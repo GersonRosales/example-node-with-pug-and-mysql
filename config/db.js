e769cf274a0f7453c65c2cf4930c884b3bf62d4a
Gerson Rosales <gerson980@gmail.com>
2018-01-20 18:33:27 -0400

    Adding connection to the DB

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const db = {
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'caol',
+  port: '3306',
+};
+
+module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../config/db');
+const mysql = require('mysql');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
+  const connection = mysql.createConnection(db);
+  let consultant;
+
+  connection.connect((error) => {
+    if (error) {
+      throw error;
+    } else {
+      console.log('Connection Successful.');
+    }
+  });
+  connection.end();
   res.render('index', { title: 'Express' });
 });
 
