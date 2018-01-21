ee84da762d47177400ec6788c6d3573c60889ec0
Gerson Rosales <gerson980@gmail.com>
2018-01-20 21:29:19 -0400

    Query for consultants and adding inputs for date and Selector for listing of consultant

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
 const mysql = require('mysql');
+const _ = require('lodash');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   const connection = mysql.createConnection(db);
-  let consultant;
 
   connection.connect((error) => {
     if (error) {
@@ -15,8 +15,26 @@ router.get('/', (req, res, next) => {
       console.log('Connection Successful.');
     }
   });
-  connection.end();
-  res.render('index', { title: 'Express' });
+
+  const textQuery = `SELECT * FROM permissao_sistema ps
+  JOIN cao_usuario cu
+  ON ps.co_usuario = cu.co_usuario
+  WHERE ps.co_sistema = 1
+  AND ps.in_ativo = 'S'
+  AND ps.co_tipo_usuario IN (0,1,2)`;
+
+  connection.query(textQuery, (error, result, fields) => {
+    if (error) {
+      throw error;
+    } else {
+      const consultant = _.map(result, 'no_usuario');
+      if (consultant.length > 0) {
+        res.render('index', { consultant: consultant });
+      } else {
+        console.log('Registro no encontrado');
+      }
+    }
+  });
 });
 
 module.exports = router;
